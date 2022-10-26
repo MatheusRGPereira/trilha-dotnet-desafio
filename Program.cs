134dcc11f68fed2c5de44ddcfc03e50f59c4ea6f
Matheus <matheusgaldinoinfo@gmail.com>
2022-10-25 21:40:47 -0300

    Subindo desafio .net DIO

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+
+Console.WriteLine(" Celular Nokia");
+Nokia nokia = new Nokia("11987915018","UAUTECNO","458164485-x",256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Wild Rift");
+
+Console.WriteLine(" Celular Iphone");
+Iphone iphone = new Iphone("11974602849","Iphone 11","54845818-y", 256 );
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Free Fire");
